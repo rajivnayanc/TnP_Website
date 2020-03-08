5cfa9901d1c9a12e42438f23ca0c7ac900bcacfe
Rajiv  Nayan Choubey <rajivnayanc@gmail.com>
2020-03-08 16:02:00 +0530

    Added Recruiters Page

--- backend_src/app/settings.py
+++ backend_src/app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'recruiters.apps.RecruitersConfig',
     'home_page.apps.HomePageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
